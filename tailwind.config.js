1d048c3cc97e7e44ebf6854ec7328e6fc533fe1b
Fernando Chagas <fnd.chagas@gmail.com>
2022-01-03 17:18:15 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Ruda", "sans-serif"],
+			},
+			colors: {
+				"facebook-blue": "#1877f2",
+				"facebook-blue-light": "#4e69a2",
+				"facebook-green": "#00a400",
+			},
+		},
+	},
+	plugins: [],
+};
